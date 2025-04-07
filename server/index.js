0e1151b9cd531fec65f802937300eeccdc33c0bd
Stanislav <stanislav.solovei@nure.ua>
2025-04-07 07:46:30 +0300

    Added connection to DB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require("bcrypt");
+const connection = require('./MySQL/mysql.js');
 
 const app = express();
 const PORT = 5000;
